0ed0aacef9b6d7ac48b29b83a8025047236388e6
Ben Homan <ben@benhoman.com>
2024-12-13 15:28:31 -0500

    add nvim

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,17 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+local opt = vim.opt
+
+opt.autowrite = false
+opt.clipboard = "unnamed"
+opt.incsearch = true
+opt.scrolloff = 8
+opt.colorcolumn = "80,90,100"
+opt.ruler = true
+
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
+
+vim.g.lazyvim_picker = "fzf" -- or "telescope"
---
+++ nvim/lua/plugins/vim-test.lua
@@ -0,0 +1,27 @@
+return {
+  "vim-test/vim-test", -- "klen/nvim-test",
+  config = function()
+    vim.keymap.set("n", "<leader>tn", ":TestNearest<cr>")
+    vim.keymap.set("n", "<leader>tf", ":TestFile<cr>")
+    vim.keymap.set("n", "<leader>tl", ":TestLast<cr>")
+    vim.keymap.set("n", "<leader>tv", ":TestVisit<cr>")
+    vim.keymap.set("n", "<leader>tc", ":TestClass<cr>")
+    vim.keymap.set("t", "<C-o>", "<C-\\><C-n>")
+
+    vim.g["test#strategy"] = "floaterm"
+    -- vim.g["test#strategy"] = "neovim"
+    -- vim.g["test#neovim#term_position"] = "vert"
+    vim.g["test#python#runner"] = "djangotest"
+    vim.g["test#python#djangotest#executable"] = "ca test"
+    vim.g["test#python#pytest#executable"] = "ca test"
+  end,
+  dependencies = {
+    {
+      "voldikss/vim-floaterm",
+      config = function()
+        vim.g["floaterm_height"] = 0.95
+        vim.g["floaterm_width"] = 0.80
+      end,
+    },
+  },
+}
